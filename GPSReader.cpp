50bd353063ee929250c8c4d5b3a8cb7c0c0b21a7
cljunggr <conny.ljunggren@ha.ax>
2014-04-22 15:31:32 +0300

    refactor

--- GPSReader.cpp
+++ GPSReader.cpp
@@ -23,39 +23,29 @@ void GPSReader::connectToGPS() {
 	system("sudo pkill gpsd; gpsd /dev/ttyUSB0");
 	system("sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock");
 
-	gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);
-
-	if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) {
-		cerr << "No GPSD running.\n";
+	m_gpsConnection = new gpsmm("localhost", DEFAULT_GPSD_PORT);
+	if (m_gpsConnection->stream(WATCH_ENABLE | WATCH_JSON) == NULL) {
+		throw "GPSReader::connectToGPS(), unable to connect to GPS.";
 	}
-	m_gpsConnection = &gps_rec;
 }
 
 void GPSReader::readGPS() {
 	struct gps_data_t* newdata;
 
 	if (!m_gpsConnection->waiting(50000000)) {
-		std::cout << "Been waiting for a long time now, pal!" << endl;
+		throw "GPSReader::readGPS(), timeout.";
 	}
 
 	if ((newdata = m_gpsConnection->read()) == NULL) {
-		cerr << "Read error.\n";
+		throw "GPSReader::readGPS(), read error.";
 	} else {
-
 		m_timestamp = secondsToTime(newdata->fix.time);
-
 		m_latitude = newdata->fix.latitude;
-
 		m_longitude = newdata->fix.longitude;
-
 		m_altitude = newdata->fix.altitude;
-
 		m_speed = newdata->fix.speed;
-
 		m_heading = newdata->fix.track;
-
 		m_mode = newdata->fix.mode;
-
 		m_satellitesUsed = newdata->satellites_used;
 	}
 }

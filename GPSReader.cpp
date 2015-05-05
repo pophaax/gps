1602a82cd17612d704f66e3f09b8e9b7429352d9
sailbot <sailbot>
2015-05-05 16:36:52 +0300

    Added four decimals of seconds to timestamp.

--- GPSReader.cpp
+++ GPSReader.cpp
@@ -18,12 +18,17 @@ GPSReader::~GPSReader() {
 }
 
 std::string GPSReader::secondsToTimeStamp(double seconds) {
-	time_t rawtime = (time_t) seconds;
+	time_t fullSeconds = static_cast<time_t>(seconds);
+	double partSecond = seconds - fullSeconds;
+	int partSecondsFourDecimals = static_cast<int>(partSecond * 10000.0 + 0.5);
 	struct tm * timeinfo;
-	timeinfo = localtime(&rawtime);
+	timeinfo = localtime(&fullSeconds);
 	char timeInfoBuffer[100];
 	strftime(timeInfoBuffer, 100, "%F %T", timeinfo);
-	return std::string(timeInfoBuffer);
+	std::string timeStamp(timeInfoBuffer);
+	timeStamp.append(".");
+	timeStamp.append(std::to_string(partSecondsFourDecimals));
+	return timeStamp;
 }
 
 void GPSReader::connectToGPS() {
